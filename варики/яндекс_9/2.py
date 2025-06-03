c409fc91026ef4353df81209dc3d0e27a6b3df7f
B-A-P-A-LLI-K-A <cdft81040@gmail.com>
2025-06-03 21:11:10 +0300

    daunism moment

--- варики/яндекс_9/2.py
+++
@@ -1,7 +0,0 @@
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                f = ((z <= x) and (not(y) and (not(w) == y)))
-                if f == 0 and y == 1 and w == 0:
-                    print(x, y, z, w, f)
